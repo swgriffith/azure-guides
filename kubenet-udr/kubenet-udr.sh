c17e2921811a0764344c12227fa0d7935b368186
Steve Griffith <steve.griffith@microsoft.com>
2020-08-25 23:16:56 -0400

    added acr file

--- kubenet-udr/kubenet-udr.sh
+++ kubenet-udr/kubenet-udr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-RG=EphAKSKubenetUDR
-ACRNAME=kubenetdemoacr
-CLUSTERNAME=kubenetudr
+RG=EphAKSKubenetUDR3
+ACRNAME=kubenetdemoacr3
+CLUSTERNAME=kubenetudr3
 LOCATION=eastus
-KEYVAULTNAME=griffkv3
+KEYVAULTNAME=griffkv4
 FIREWALLNAME=aks-firewall
+LA_WORKSPACE_NAME=grifflademo
 
 # Create Resource Group
 az group create -n $RG -l eastus
@@ -31,12 +32,6 @@ az network vnet subnet create \
     --name acrSubnet \
     --address-prefix 10.40.2.0/24
     
-az network vnet subnet update \
-    --resource-group $RG \
-    --vnet-name aksvnet \
-    --name acrSubnet \
-    --disable-private-endpoint-network-policies
-
 # Create Azure Firewall Public IP
 az network public-ip create -g $RG -n azfirewall-ip -l $LOCATION --sku "Standard"
 
@@ -116,16 +111,15 @@ az network firewall application-rule create \
 --action allow --priority 100
 
 # Docker Hub Rules
-# az network firewall application-rule create \
-# -g $RG \
-# -f $FIREWALLNAME \
-# --collection-name 'aksfwar' \
-# -n 'fqdn' \
-# --source-addresses '*' \
-# --protocols 'http=80' 'https=443' \
-# --target-fqdns "auth.docker.io,registry-1.docker.io,index.docker.io" \
-# --action allow --priority 100
-
+az network firewall application-rule create \
+-g $RG \
+-f $FIREWALLNAME \
+--collection-name 'aksfwdocker' \
+-n 'docker' \
+--source-addresses '*' \
+--protocols 'http=80' 'https=443' \
+--target-fqdns auth.docker.io registry-1.docker.io index.docker.io dseasb33srnrn.cloudfront.net production.cloudflare.docker.com \
+--action allow --priority 101
 
 # Associate Route Table to AKS Subnet
 az network vnet subnet update \
@@ -162,82 +156,13 @@ az keyvault set-policy -n $KEYVAULTNAME -g $RG --object-id $desIdentity --key-pe
 # Retrieve the DiskEncryptionSet value and set a variable
 diskEncryptionSetId=$(az disk-encryption-set show -n aksDiskEncryptionSetName -g $RG --query id -o tsv)
 
+# Create Log Analytics Workspace
+az monitor log-analytics workspace create \
+--resource-group $RG \
+--workspace-name $LA_WORKSPACE_NAME
 
-
-# Create an Azure Container Registry
-az acr create \
--g $RG \
--n $ACRNAME \
---sku Premium \
---public-network-enabled false
-
-# Create Private Link for ACR
-az network private-dns zone create \
-  --resource-group $RG \
-  --name "kubenetdemo.azurecr.io"
-
-# Associate private dns with vnet
-az network private-dns link vnet create \
-  --resource-group $RG \
-  --zone-name "kubenetdemo.azurecr.io" \
-  --name MyDNSLink \
-  --virtual-network aksvnet \
-  --registration-enabled false
-
-# Get Registry ID
-REGISTRY_ID=$(az acr show --name $ACRNAME --query 'id' --output tsv)
-
-az network private-endpoint create \
-    --name myPrivateEndpoint \
-    --resource-group $RG \
-    --vnet-name aksvnet \
-    --subnet acrSubnet \
-    --private-connection-resource-id $REGISTRY_ID \
-    --group-ids registry \
-    --connection-name myConnection
-
-NETWORK_INTERFACE_ID=$(az network private-endpoint show \
-  --name myPrivateEndpoint \
-  --resource-group $RG \
-  --query 'networkInterfaces[0].id' \
-  --output tsv)
-
-az network private-dns record-set a create \
-  --name $ACRNAME \
-  --zone-name kubenetdemo.azurecr.io \
-  --resource-group $RG
-
-# Specify registry region in data endpoint name
-az network private-dns record-set a create \
-  --name ${ACRNAME}.${LOCATION}.data \
-  --zone-name kubenetdemo.azurecr.io \
-  --resource-group $RG
-
-  PRIVATE_IP=$(az resource show \
-  --ids $NETWORK_INTERFACE_ID \
-  --api-version 2019-04-01 \
-  --query 'properties.ipConfigurations[1].properties.privateIPAddress' \
-  --output tsv)
-
-  DATA_ENDPOINT_PRIVATE_IP=$(az resource show \
-  --ids $NETWORK_INTERFACE_ID \
-  --api-version 2019-04-01 \
-  --query 'properties.ipConfigurations[0].properties.privateIPAddress' \
-  --output tsv)
-
-
-  az network private-dns record-set a add-record \
-  --record-set-name $ACRNAME \
-  --zone-name kubenetdemo.azurecr.io \
-  --resource-group $RG \
-  --ipv4-address $PRIVATE_IP
-
-# Specify registry region in data endpoint name
-az network private-dns record-set a add-record \
-  --record-set-name ${ACRNAME}.${LOCATION}.data \
-  --zone-name kubenetdemo.azurecr.io \
-  --resource-group $RG \
-  --ipv4-address $DATA_ENDPOINT_PRIVATE_IP
+# Get Workspace ID
+LA_WORKSPACE_ID=$(az monitor log-analytics workspace show -g $RG -n $LA_WORKSPACE_NAME -o tsv --query id)
 
 # Create the AKS Cluster
 az aks create \
@@ -248,8 +173,28 @@ az aks create \
 --node-osdisk-diskencryptionset-id $diskEncryptionSetId \
 --enable-aad \
 --enable-addons monitoring \
---attach-acr $ACRNAME \
+--workspace-resource-id "$LA_WORKSPACE_ID" \
 --outbound-type userDefinedRouting 
 
-# Get Cluster Credentials
+# Get Cluster Admin Credentials
+az aks get-credentials -g $RG -n $CLUSTERNAME --admin
+
+# Add admin role binding for user
+cat << EOF | kubectl apply -f -
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: griff-admin
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: User
+  name: "stgriffi@microsoft.com"
+EOF
+
+# Get non-admin credential
 az aks get-credentials -g $RG -n $CLUSTERNAME
+
+
