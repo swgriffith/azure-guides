98ba15d853c8ee74a49bcb848b171bbacf1a10ea
Steve Griffith <steve.griffith@microsoft.com>
2020-09-24 10:20:19 -0400

    added kubelogin example

--- aad-auth-demo/cleanup.sh
+++ aad-auth-demo/cleanup.sh
@@ -3,8 +3,9 @@
 RG=EphAADDemos
 
 az group delete -n $RG -y --no-wait
-
+az ad sp delete --id $(cat ./temp/sp.json| jq -r .appId)
 rm ~/.kube/config
 
 rm -rf ./temp
 rm -rf ./certs
+
--- aad-auth-demo/create-clusters.sh
+++ aad-auth-demo/create-clusters.sh
@@ -15,5 +15,6 @@ az aks get-credentials -g $RG -n $CLUSTERNAME --admin
 # Create non-AAD Cluster
 az aks create -g $RG -n $CLUSTERNAME-aad -c 1 --enable-aad
 az aks get-credentials -g $RG -n $CLUSTERNAME-aad
+az aks get-credentials -g $RG -n $CLUSTERNAME-aad --admin
 
 kubectl config use-context $CLUSTERNAME
