a99e5d9cb4650095d4e30d3eb65c93f4ee21fca7
Steve Griffith <stgriffi@microsoft.com>
2021-03-17 21:54:54 -0400

    Update 1_create-clusters.sh
    fixed comment

--- aad-auth-demo/1_create-clusters.sh
+++ aad-auth-demo/1_create-clusters.sh
@@ -12,7 +12,7 @@ az aks create -g $RG -n $CLUSTERNAME -c 1
 az aks get-credentials -g $RG -n $CLUSTERNAME
 az aks get-credentials -g $RG -n $CLUSTERNAME --admin
 
-# Create non-AAD Cluster
+# Create AAD Cluster
 echo "Create AAD Enabled Cluster"
 az aks create -g $RG -n $CLUSTERNAME-aad -c 1 --enable-aad
 az aks get-credentials -g $RG -n $CLUSTERNAME-aad
