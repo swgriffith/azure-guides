5d2ac9cebada1448f2f54120dd274d0e285b4641
Steve Griffith <stgriffi@microsoft.com>
2021-06-10 16:52:49 -0400

    Update get-kube-config.sh

--- temp/get-kube-config.sh
+++ temp/get-kube-config.sh
@@ -5,13 +5,10 @@ ssh -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST 'sudo cp /etc/rancher/k3s/
 mkdir .kube
 scp -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST:~/k3s.yaml ./.kube/config
 chmod 744 ~/.kube/config
-sed -i 's/127.0.0.1/'$CONTROL_HOST'/g' config 
+sed -i 's/127.0.0.1/'$CONTROL_HOST'/g' ./.kube/config 
 
 # Wait for cloud-init to finish
-while [  $(cat /tmp/cloud-init.status) != 'Ready' ]; do
-    echo 'Not Ready'
-    sleep 5
-done
+while [ ! -f /tmp/cloud-init-done ]; do echo 'Not Ready';sleep 5; done
 
 # Login to Azure with the vm managed identity
 az login --identity
