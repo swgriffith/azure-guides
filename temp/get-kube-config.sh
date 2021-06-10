c2e82c8af3417309f896035ae8716b2b3d7b15b0
Steve Griffith <stgriffi@microsoft.com>
2021-06-10 16:36:41 -0400

    Update get-kube-config.sh

--- temp/get-kube-config.sh
+++ temp/get-kube-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-ssh -o "StrictHostKeyChecking no" $USER@$1 'sudo cp /etc/rancher/k3s/k3s.yaml ~/;sudo chown $USER:$USER k3s.yaml'
+export CONTROL_HOST = $1
+ssh -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST 'sudo cp /etc/rancher/k3s/k3s.yaml ~/;sudo chown $USER:$USER k3s.yaml'
 mkdir .kube
-scp -o "StrictHostKeyChecking no" $USER@$1:~/k3s.yaml ./.kube/config
+scp -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST:~/k3s.yaml ./.kube/config
 chmod 744 ~/.kube/config
-sed -i 's/127.0.0.1/$1/g' config 
+sed -i 's/127.0.0.1/'$CONTROL_HOST'/g' config 
 
 # Wait for cloud-init to finish
 while [  $(cat /tmp/cloud-init.status) != 'Ready' ]; do
