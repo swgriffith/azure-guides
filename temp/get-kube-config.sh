58e62af834c2f3fd3be8936ccf5d06ba305ef9f0
Steve Griffith <stgriffi@microsoft.com>
2021-06-11 12:02:46 -0400

    Update get-kube-config.sh
    added connectedk8s extension install

--- temp/get-kube-config.sh
+++ temp/get-kube-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#CONTROL_HOST=$1
 ssh -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST 'sudo cp /etc/rancher/k3s/k3s.yaml ~/;sudo chown $USER:$USER k3s.yaml'
 mkdir .kube
 scp -o "StrictHostKeyChecking no" $USER@$CONTROL_HOST:~/k3s.yaml ./.kube/config
@@ -11,4 +11,4 @@ while [ ! -f /tmp/cloud-init-done ]; do echo 'Not Ready';sleep 5; done
 
 # Login to Azure with the vm managed identity
 az login --identity
-
+az extension add --name connectedk8s
