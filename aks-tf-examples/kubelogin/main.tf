0766ea2b906c705e07e847fbf2626dd261d0ce08
Steve Griffith <stgriffi@microsoft.com>
2023-07-13 13:18:08 -0400

    added kubelogin example, in progress

---
+++ aks-tf-examples/kubelogin/main.tf
@@ -0,0 +1,131 @@
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
+  prefix = "dns"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = azurerm_resource_group.rg.location
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  
+  //Azure AD Setup
+  local_account_disabled = true
+
+  azure_active_directory_role_based_access_control {
+  managed = true
+  admin_group_object_ids = var.admin_group_object_ids
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = var.node_count
+    vnet_subnet_id = var.vnet_subnet_id    
+  }
+
+  private_cluster_enabled = false
+
+  network_profile {
+    network_plugin      = var.network_profile.network_plugin
+    network_plugin_mode = var.network_profile.network_plugin_mode
+    network_policy      = var.network_profile.network_policy
+    load_balancer_sku   = var.network_profile.load_balancer_sku
+    outbound_type       = var.network_profile.outbound_type
+    service_cidr        = var.network_profile.service_cidr
+    service_cidrs       = var.network_profile.service_cidrs
+    dns_service_ip      = var.network_profile.dns_service_ip
+    pod_cidr            = var.network_profile.pod_cidr
+    pod_cidrs           = var.network_profile.pod_cidrs
+    ip_versions         = var.network_profile.ip_versions
+    ebpf_data_plane     = var.network_profile.ebpf_data_plane
+  }
+}
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.k8s.kube_config[0].host
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config[0].cluster_ca_certificate)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command = "kubelogin"
+    args = [
+      "get-token",
+      "--login",
+      "spn",
+      "--environment",
+      "AzurePublicCloud",
+      "--tenant-id",
+      var.tenant_id,
+      "--server-id",
+      var.client_id,
+      "--client-id",
+      var.client_id,
+      "--client-secret",
+      var.client_secret
+    ]
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  provider = kubernetes
+  metadata {
+    name = "scalable-nginx-example"
+    labels = {
+      App = "ScalableNginxExample"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
