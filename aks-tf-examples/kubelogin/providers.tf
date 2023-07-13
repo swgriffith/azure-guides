5cb12cdfa83c618d46a84407434066996f0b4896
Raymond Kao <ray.kao@microsoft.com>
2023-07-13 18:45:57 -0400

    add: azuread provider to grab AKS enabled RBAC spn id
    Preparing to fix issue with using kubelogin get-token command to grab proper k8s auth token with cluster.  The SPN for the --server-id value is not the same as the --client-id (var.client_id)

--- aks-tf-examples/kubelogin/providers.tf
+++ aks-tf-examples/kubelogin/providers.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    azuread = {
+      source = "hashicorp/azuread"
+      version = "2.39.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~>3.0"
